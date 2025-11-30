b9f884b24d9164b63f3c2450314e246f0b906a93
MrBased <felipeaguilarwenig@uc.cl>
2025-11-30 02:56:38 -0300

    fix row height

--- src/components/Metrics/DetailedMetricEntry.tsx
+++ src/components/Metrics/DetailedMetricEntry.tsx
@@ -153,10 +153,10 @@ export default function DetailedMetricEntry({
           </span>
         </p>
 
-        <p>
+        <p className="flex items-center h-8">
           {!episodeValidation.chief_validation ? (isChief ? (
             <button
-              className="rounded-xl px-6 py-2 text-white shadow bg-[var(--color-secondary)] hover:bg-[var(--color-secondary-hover)]"
+              className="rounded-xl px-6 py-1 text-sm text-white shadow bg-[var(--color-secondary)] hover:bg-[var(--color-secondary-hover)]"
               onClick={(e) => {
                 e.stopPropagation();
                 onValidate(episodeValidation);
@@ -164,7 +164,7 @@ export default function DetailedMetricEntry({
             >
               Validar Decisión
             </button>
-          ) : <span>Solo un jefe de turno puede validar</span>) : (
+          ) : <span className="text-sm">Solo un jefe de turno puede validar</span>) : (
             <span
               className={`rounded-full px-3 py-1 text-sm font-semibold text-center bg-${colorChief}-200 text-${colorChief}-700`}
             >
